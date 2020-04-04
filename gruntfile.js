1702658587edd7f5a10b53a93454d7e0d7215b4a
RamkumarK <slashnot84@gmail.com>
2020-04-04 16:37:21 +0530

    Changed output targets and modules

--- gruntfile.js
+++ gruntfile.js
@@ -3,17 +3,17 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
     projectFiles: ['src/litegraph.js',
       'src/nodes/base.js',
-      'src/nodes/events.js',
-      'src/nodes/interface.js',
-      'src/nodes/input.js',
-      'src/nodes/math.js',
-      'src/nodes/logic.js',
-      'src/nodes/image.js',
-      'src/nodes/gltextures.js',
-      'src/nodes/glfx.js',
-      'src/nodes/midi.js',
-      'src/nodes/audio.js',
-      'src/nodes/network.js'
+      // 'src/nodes/events.js',
+      // 'src/nodes/interface.js',
+      // 'src/nodes/input.js',
+      // 'src/nodes/math.js',
+      // 'src/nodes/logic.js',
+      // 'src/nodes/image.js',
+      // 'src/nodes/gltextures.js',
+      // 'src/nodes/glfx.js',
+      // 'src/nodes/midi.js',
+      // 'src/nodes/audio.js',
+      // 'src/nodes/network.js'
     ],
     concat: {
       build: {
